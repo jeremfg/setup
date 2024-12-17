db5fcfa4ed773b8987909f51213f19e6ee4ffc13
Jérémie Faucher-Goulet <jeremiefauchergoulet@hotmail.com>
2024-12-17 18:12:45 -0500

    Small typo

--- src/ssh.sh
+++ src/ssh.sh
@@ -64,7 +64,7 @@ ssh-add -l &>/dev/null
 if [[ "\$?" == 2 ]]; then
   test -r ~/.ssh-agent && eval "\$(<~/.ssh-agent)" >/dev/null
   ssh-add -l &>/dev/null
-  if [ "\$?" == 2 ]; then
+  if [[ "\$?" == 2 ]]; then
     (umask 066; ssh-agent > ~/.ssh-agent)
     eval "\$(<~/.ssh-agent)" >/dev/null
     ssh-add
